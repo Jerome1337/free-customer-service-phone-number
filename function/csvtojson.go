34ff818852db3b835140661ab3d2c30a8fcaae89
Jérôme Pogeant <p-jerome@hotmail.fr>
2022-02-10 18:17:51 +0100

    fix cloud function returns ans phone number formating

--- function/csvtojson.go
+++ function/csvtojson.go
@@ -2,15 +2,13 @@ package function
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 )
 
 // Number represent a chargeable or free phone number with its additional information
 type Number struct {
-	Number                int    `json:"number"`
+	Number                string    `json:"number"`
 	AdditionalInformation string `json:"additional_information,omitempty"`
 }
 
@@ -19,16 +17,6 @@ type Service struct {
 	Name              string   `json:"name"`
 	ChargeableNumbers []Number `json:"chargeable_numbers"`
 	FreeNumbers       []Number `json:"free_numbers"`
-}
-
-func transformPhoneNumberToInt(phoneNumber string) int {
-	number, err := strconv.Atoi(phoneNumber)
-
-	if err != nil {
-		return 0
-	}
-
-	return number
 }
 
 func formatServicesNumbers(data [][]string) []Service {
@@ -61,14 +49,14 @@ func formatServicesNumbers(data [][]string) []Service {
 
 				previousName = field
 			} else if j == 1 {
-				chargeableFormattedNumber := transformPhoneNumberToInt(field)
+				chargeableFormattedNumber := field
 
-				if chargeableFormattedNumber != 0 {
+				if chargeableFormattedNumber != "0" {
 					isChargeableNumberEmpty = false
 					chargeableNumber.Number = chargeableFormattedNumber
 				}
 			} else if j == 2 {
-				freeNumber.Number = transformPhoneNumberToInt(field)
+				freeNumber.Number = field
 			} else if j == 3 {
 				freeNumber.AdditionalInformation = field
 			}
@@ -90,8 +78,7 @@ func formatServicesNumbers(data [][]string) []Service {
 	return services
 }
 
-// FormatJSON gets the CSV file from GitHub then return a the JSON representation byte slice
-func FormatJSON() ([]byte, error) {
+func formatJSON() ([]Service, error) {
 	response, err := http.Get("https://raw.githubusercontent.com/Jerome1337/free-customer-service-phone-number/main/data-files/phones-numbers.csv")
 	if err != nil {
 		return nil, fmt.Errorf("Failed to get CSV file", err)
@@ -107,12 +94,5 @@ func FormatJSON() ([]byte, error) {
 		return nil, fmt.Errorf("Failed to read CSV content", err)
 	}
 
-	services := formatServicesNumbers(data)
-
-	jsonData, err := json.Marshal(services)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to marshal json value", err)
-	}
-
-	return jsonData, nil
+	return formatServicesNumbers(data), nil
 }
