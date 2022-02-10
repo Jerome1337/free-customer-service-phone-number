34863c19cd96881c64d858c97f6dcc910bee3d9c
Jérôme Pogeant <p-jerome@hotmail.fr>
2022-02-10 18:44:36 +0100

    configure cors into cloud function (#5)

--- function/handler.go
+++ function/handler.go
@@ -7,6 +7,17 @@ import (
 
 // Handler is the cloud function entrypoint
 func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+
+		return
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 
 	data, err := formatJSON()
